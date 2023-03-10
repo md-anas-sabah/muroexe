0241e04d83ae81132c0ca62f3ed2344e28fd78e3
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-10 16:56:00 +0530

    update: integrated razorpay payment gateway

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CheckoutCard from "../components/CheckoutCard";
 import { closeCart } from "../redux/toggleCartSlice";
+import PaymentModal from "../Payment/PaymentModal";
 
 const Checkout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [price, setPrice] = useState(0);
+
   const dispatch = useDispatch();
   const Navigate = useNavigate();
   useEffect(() => {
     dispatch(closeCart());
   });
+
   const { items } = useSelector((store) => store.cart);
 
   if (items.length < 1) {
@@ -22,15 +27,29 @@ const Checkout = () => {
     }, 0);
   }, [items]);
 
+  const buyMovies = () => {
+    setIsOpen(true);
+    setPrice(totalPrice);
+  };
+
   return (
     <div className="flex flex-col mt-14 w-10/12 ml-auto mr-auto border shadow-2xl">
+      <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price} />
       <div className="flex flex-col items-center gap-3 ">
         {items.map((item) => {
           return <CheckoutCard item={item} key={item.data.id} />;
         })}
       </div>
-      <div className="">
-        <h1>Total: ₹{totalPrice} </h1>
+      <div className="flex flex-col p-10">
+        <h1 className="font-roboto text-xl font-extrabold">
+          Total:{" "}
+          <span className="font-michroma font-semibold text-red-900">
+            ₹{totalPrice}
+          </span>
+        </h1>
+        <div>
+          <button onClick={buyMovies} className="bg-black w-20 mt-3 h-10 text-white rounded-lg font-roboto font-bold">Pay</button>
+        </div>
       </div>
     </div>
   );
